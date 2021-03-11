35d3dff786c86242d4dcb39e7fba28780416b987
snow6098 <77281784+zam6098@users.noreply.github.com>
2021-03-11 11:07:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ cd ~
 
 echo "Installing sqlid"
 sudo apt install -y jq
-git clone https://github.com/zam6098/sqlid.git
 cd sqlid/bin
-mv * /usr/local/bin
+sudo mv * /usr/local/bin
 
 mv -r sqlid /opt/
-
-cd /usr/local/bin
+cd /opt/sqlid
 chmod +x sqlid.sh
+chmod +x uDork.sh
+cd /usr/local/bin
 sudo ln -s /opt/sqlid/sqlid.sh sqlid
 sudo ln -s /opt/sqlid/uDork.sh udork
